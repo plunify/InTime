267b61600fce1b5494884112c970c60dc0029dc7
Ng Harn Hua <harnhua@users.noreply.github.com>
2019-05-15 10:06:04 +0800

    Moved scripts to their respective folders in scripts/

--- intimeflow.tcl
+++
@@ -1,8 +0,0 @@
-# Sample script for InTime Optimization. Works Windows & Linux
-flow reset
-flow set run_target local
-flow set runs_per_round 2
-flow set rounds 1
-flow set concurrent_runs 1
-flow set control_create_bitstreams false
-flow run_recipe "hot_start"
