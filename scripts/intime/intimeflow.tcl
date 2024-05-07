81dced55dfaa89229a99ef3619025fcc43f8ccc8
Harn Hua Ng <harnhua@users.noreply.github.com>
2024-05-07 17:07:59 +0800

    Added command to load recipe defaults

--- scripts/intime/intimeflow.tcl
+++ scripts/intime/intimeflow.tcl
@@ -1,8 +1,13 @@
-# Sample script for InTime Optimization. Works Windows & Linux
+# Sample script for InTime Optimization
+# Supports Windows & Linux environments
+
 flow reset
+flow load_recipe "hot_start"
+# Compile on the same machine (local) / grid (private_cloud) / AWS (plunify_cloud)
 flow set run_target local
+# Number of strategies to generate
 flow set runs_per_round 2
-flow set rounds 1
+# Number of compilations to run in parallel
 flow set concurrent_runs 1
 flow set control_create_bitstreams false
 flow run_recipe "hot_start"
